8f37edc3885b71f8ffe686af866291f3d62c54d1
krishnaSrikar567 <gvnpsks@gmail.com>
2024-06-26 16:06:38 +0530

    router

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header/>
+      <Home/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
